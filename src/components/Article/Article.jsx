0c6fc0f98914baca2860205b724e0f95fff9c2a4
eric.oswald <oswald.eric99@gmail.com>
2023-06-23 12:05:09 +0200

    added rating module

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import FormulaireCommentaire from './FormulaireCommentaire'
-import Evaluation from './Evaluation'
+import FormulaireCommentaire from "./FormulaireCommentaire";
+import Evaluation from "./Evaluation";
 import { useParams } from "react-router-dom";
-import {articles} from "../../Api";
+import { articles } from "../../Api";
 
-    const Article = (props) => {
-console.log("mounted")
-        const {id} = useParams();
-        console.log("articles", articles)
-        const currentArticle = articles.find((article) => article.id === parseInt(id));
+const Article = (props) => {
+  console.log("mounted");
+  const { id } = useParams();
+  console.log("articles", articles);
+  const currentArticle = articles.find(
+    (article) => article.id === parseInt(id)
+  );
 
-        return(
-            <div>
-                <h1>{currentArticle.title}</h1>
-                <br />
-                <FormulaireCommentaire currentArticle={currentArticle} />
-                <Evaluation />
-            
-
-            </div>
-        )
-    }
+  return (
+    <div>
+      <h1>{currentArticle.title}</h1>
+      <br />
+      <FormulaireCommentaire currentArticle={currentArticle} />
+      <Evaluation />
+    </div>
+  );
+};
 
 export default Article;
--- src/components/Article/Evaluation.jsx
+++ src/components/Article/Evaluation.jsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
-    const Evalutation = (props) => {
-        return(<div>
-            
-        </div>)
-    }
+const RatingStar = ({ initialRating, onRatingChange }) => {
+  const [rating, setRating] = useState(initialRating);
 
-export default Evalutation;
+  const handleClick = (newRating) => {
+    setRating(newRating);
+    onRatingChange(newRating);
+  };
+
+  return (
+    <div>
+      {[...Array(5)].map((_, index) => {
+        const starRating = index + 1;
+        return (
+          <span
+            key={starRating}
+            onClick={() => handleClick(starRating)}
+            onMouseEnter={() => setRating(starRating)}
+            onMouseLeave={() => setRating(initialRating)}
+            style={{
+              cursor: "pointer",
+              color: starRating <= rating ? "gold" : "gray",
+              fontSize: "1.5rem",
+            }}
+          >
+            {starRating <= rating ? "★" : "☆"}
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+const Evaluation = ({ currentArticle }) => {
+  console.log("hello", currentArticle);
+
+  const [articleRatings, setArticleRatings] = useState(0);
+
+  const handleRatingChange = (rating) => {
+    setArticleRatings(rating);
+
+    const article = { ...currentArticle };
+    const oldAverageRating = article.rating;
+    const numberOfVotes = article.numberOfRating;
+    const newAverageRating =
+      (oldAverageRating * numberOfVotes + rating) / (numberOfVotes + 1);
+    console.log(newAverageRating);
+
+    currentArticle.rating = newAverageRating;
+    article.numberOfRating += 1;
+
+    console.log(article);
+  };
+
+  return (
+    <div>
+      <div key={currentArticle.id}>
+        <RatingStar
+          initialRating={articleRatings}
+          onRatingChange={(rating) => handleRatingChange(rating)}
+        />
+        {articleRatings > 0 && (
+          <p>
+            Voici la note moyenne de cet article :
+            {Math.round(currentArticle.rating * 100) / 100}{" "}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Evaluation;
